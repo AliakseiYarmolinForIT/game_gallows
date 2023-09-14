2f26c5a8f0e60b36e5bc2de0f3122e9091ed2d99
AliakseiYarmolin <aliakseiyarmolinforit@gmail.com>
2023-09-14 14:04:09 +0300

    game code

--- game_code.js
+++ game_code.js
@@ -1,38 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Моя первая настоящая HTML-страничка</title>
+    <meta charset="UTF-8">
+    <title>Виселица!</title>
 </head>
 
 <body>
-<h1>Привет, мир!</h1>
-<p>Моя первая веб-страничка.</p>
-<script>
-var name = prompt("Как Вас зовут?");
-console.log("Привет, " + name);
-var words = [
-"программа",
-"макака",
-"прекрасный",
-"оладушек"
-]
-var word = words[Math.floor(Math.random() * words.length)];
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-answerArray[i] = "_";
-}
-var remainingLetters = word.length;
-while (remainingLetters > 0) {
-alert(answerArray.join(" "));
-var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
-if (guess === null) {
-break;
-} else if (guess.length !== 1) {
-alert ("Пожалуйста, введите только одну букву.");
-} else {
+    <h1>Виселица!</h1>
 
-}
-</script>
+    <script>
+        var name = prompt("Как Вас зовут?");
+        alert("Привет, " + name);
+        var words = [
+            "программа",
+            "макака",
+            "прекрасный",
+            "оладушек"
+        ];
+
+        var word = words[Math.floor(Math.random() * words.length)];
+
+        var answerArray = [];
+        for (var i = 0; i < word.length; i++) {
+            answerArray[i] = "_";
+        }
+
+        var remainingLetters = word.length;
+
+        while (remainingLetters > 0) {
+            alert(answerArray.join(" "));
+            var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+            if (guess === null) {
+                break;
+            } else if (guess.length !== 1) {
+                alert ("Пожалуйста, введите только одну букву.");
+            } else {
+                for (var J = 0; j < word.length; j++) {
+                    if (word[j] === guess) {
+                        answerArray[j] = guess;
+                        remainingLetters--;
+                    }
+                }
+            }
+        }
+    alert(answerarray.join(" "));
+    alert("Отлично! Было загадано слово " + word);
+    </script>
 </body>
 </html>
